ac50222f8add823e72f49050501e3be7c041c764
ssarick <66787805+ssarick@users.noreply.github.com>
2020-12-28 16:30:09 +0500

    demo 2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ new Splide('.splide', {
     autoWidth: true,
     focus: 'center',
 }).mount();
+
+
+var endTime = new Date(2021, 0, 10).getTime() / 1000;
+
+function setClock() {
+    var elapsed = new Date().getTime() / 1000;
+    var totalSec = endTime - elapsed;
+    var d = parseInt(totalSec / 86400);
+    var h = parseInt(totalSec / 3600) % 24;
+    var m = parseInt(totalSec / 60) % 60;
+    var s = parseInt(totalSec % 60, 10);
+
+    $('.days').text(d);
+    $('.hours').text(h);
+    $('.minutes').text(m);
+    $('.seconds').text(s);
+    setTimeout(setClock, 1000);
+}
+setClock();
